e0d180a617267c008bb24a6bd44badfa222226cb
Lyddem <lydiademi@gmail.com>
2017-07-24 17:02:49 -0700

    Finished up through _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,8 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(n > array.length){return array; }
+    return n === undefined ? array[array.length-1] : array.slice(array.length-n, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +48,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i],i,collection)
+      }
+    } else {
+      for(var key in collection){
+        iterator(collection[key],key,collection)
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,18 +78,32 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var final = [];
+    _.each(collection, function(item,index){
+      if(test(item)){
+        final.push(item);
+      }
+    })
+      return final;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var final = [];
+    _.each(collection, function(item, index){
+      if(!test(item)){
+        final.push(item);
+      }
+    })
+      return final;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    return array.slice();
   };
-
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
